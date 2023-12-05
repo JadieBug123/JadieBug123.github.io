5c0bb24b054b76a36e34f247c3b6cbe037e50315
Jade Boudreaux <jade.boudreaux@jpschools.org>
2023-12-05 01:05:22 +0000

    Change 2

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,23 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+           var circle; 
+           var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+        circle = draw.randomCircleInArea(canvas, true, true, "#999", 2 ); 
+        physikz.addRandomVelocity(circle, canvas);
+        view.addChild(circle); 
+        circles.push(circle); 
+        }
 
-        // TODO 3 / 7 : Call the drawCircle() function 
-
-
+        // TODO 3 / 7 : Call the drawCircle() function
+        drawCircle(); 
+        drawCircle(); 
+        drawCircle();  
+        drawCircle();
+        drawCircle();
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -39,13 +48,24 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+            physikz.updatePosition(circles[0]);
+            physiks.updatePosition(circles[1]); 
+            physiks.updatePosition(circles[2]); 
+            physiks.updatePosition(circles[3]); 
+            physikz.updatePosition(circles[4]); 
+            }
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
-
+           game.checkCirclePosition(circles[0]); 
+           game.checkCirclePosition(circles[1]); 
+           game.checkCirclePosition(circles[2]); 
+           game.checkCirclePosition(circles[3]); 
+           game.checkCirclePosition(circles[4]); 
             // TODO 9 : Iterate over the array
-           
+           function update() { 
+           for (var i = 0; i < circles.length; i++) { 
+           physiks.updatePosition(circles[i]); 
+           game.checkCirclePosition(circles[i]); 
             
         }
     
@@ -63,8 +83,11 @@ var init = function (window) {
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             
-
-
+            game.checkCirclePosition(circles[0]); 
+            game.checkCirclePosition(circles[1]); 
+            game.checkCirclePosition(circles[2]); 
+            game.checkCirclePosition(circles[3]); 
+            game.checkCirclePosition(circles[4]); 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
