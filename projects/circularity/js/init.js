db563e487256fa7cef53f10c49f86222a720c5ab
Jade Boudreaux <jade.boudreaux@jpschools.org>
2023-12-06 19:10:59 +0000

    change

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -74,11 +74,11 @@ var init = function (window) {
         Function. If that circle drifts off the screen, this Function should move
         it to the opposite side of the screen.
         */
-        game.checkCirclePosition = function(circles) {
+        game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circles.x > canvas.width) {
-                circles.x = 0;
+            if ( circle.x > canvas.width) {
+                circle.x = 0;
             } 
         } 
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
@@ -89,9 +89,9 @@ var init = function (window) {
             game.checkCirclePosition(circles[3]); 
             game.checkCirclePosition(circles[4]);*/
 
-            game.checkCirclePosition = function(circles) {
-                if (circles.x > canvas.width) {
-                    circles.x = 0; 
+            game.checkCirclePosition = function(circle) {
+                if (circle.x > canvas.width) {
+                    circle.x = 0; 
                 }
             }
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
